324097b474b94311302cbb39ae7dc41e4a806d3e
rmil <rhys.milling@googlemail.com>
2020-07-19 23:15:55 +0100

    Video statistics

--- services/creator/stats.go
+++ services/creator/stats.go
@@ -2,18 +2,41 @@ package creator
 
 import (
 	"context"
+	"log"
 
 	"github.com/ystv/web-api/utils"
 )
 
 type SQLStats struct {
-	TotalVideoHits int `db:"hits" json:"hits"`
+	TotalVideos        int `db:"videos" json:"totalVideos"`
+	TotalPendingVideos int `db:"pending" json:"totalPendingVideos"`
+	TotalVideoHits     int `db:"hits" json:"totalVideoHits"`
+	TotalStorageUsed   int `db:"storage" json:"totalStorageUsed"`
 }
 
 // Stats returns general information about video library
 func Stats(ctx context.Context) (*SQLStats, error) {
 	s := &SQLStats{}
-	err := utils.DB.GetContext(ctx, &s, `SELECT`)
+	err := utils.DB.GetContext(ctx, &s.TotalVideos,
+		`SELECT COUNT(*)
+		FROM video.items;`)
+	log.Print(err)
+	if err != nil {
+		return nil, err
+	}
+	err = utils.DB.GetContext(ctx, &s.TotalPendingVideos,
+		`SELECT COUNT(*)
+		FROM video.items
+		WHERE status = 'pending';`)
+	err = utils.DB.GetContext(ctx, &s.TotalVideoHits,
+		`SELECT COUNT(*)
+		FROM public.video_hits;`)
+	if err != nil {
+		return nil, err
+	}
+	err = utils.DB.GetContext(ctx, &s.TotalStorageUsed,
+		`SELECT SUM(size)
+		FROM video.files;`)
 	if err != nil {
 		return nil, err
 	}
