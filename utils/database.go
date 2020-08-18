191a2db1c3a3e7e5df455b69081cb61c337f2ab1
rmil <rhys.milling@googlemail.com>
2020-08-18 16:59:07 +0100

    Database transaction wrapper

--- utils/database.go
+++ utils/database.go
@@ -38,3 +38,23 @@ func InitDB() {
 
 	log.Printf("Connected to DB: %s@%s", dbName, dbHost)
 }
+
+// Transact wraps transactions
+func Transact(db *sqlx.DB, txFunc func(*sqlx.Tx) error) (err error) {
+	tx, err := db.Beginx()
+	if err != nil {
+		return
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			tx.Rollback() // err is non-nil; don't chang eit
+		} else {
+			err = tx.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+	err = txFunc(tx)
+	return err
+}
