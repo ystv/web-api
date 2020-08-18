a32222aae3bdfef20bc9941cfa44ae7ddf840b2d
rmil <rhys.milling@googlemail.com>
2020-08-18 17:31:35 +0100

    Fixed preset listing

--- services/creator/encode/preset.go
+++ services/creator/encode/preset.go
@@ -1,6 +1,8 @@
 package encode
 
 import (
+	"log"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/ystv/web-api/utils"
 	"gopkg.in/guregu/null.v4"
@@ -10,7 +12,7 @@ import (
 // it can generate the video files so a video item.
 type Preset struct {
 	PresetID    int         `json:"id" db:"id"`
-	Name        string      `json:"name" db:"description"`
+	Name        string      `json:"name" db:"name"`
 	Description null.String `json:"description" db:"description"`
 	Formats     []Format    `json:"formats"`
 }
@@ -18,13 +20,21 @@ type Preset struct {
 // PresetList returns all presets
 func PresetList() ([]Preset, error) {
 	p := []Preset{}
-	utils.DB.Select(&p, `SELECT id, name, description
+	err := utils.DB.Select(&p, `SELECT id, name, description
 						FROM video.presets;`)
+	if err != nil {
+		log.Printf("PresetList failed selected meta %v", err)
+		return nil, err
+	}
 	for i := range p {
-		utils.DB.Select(&p[i].Formats,
+		err = utils.DB.Select(&p[i].Formats,
 			`SELECT format.id, name, description, mime_type, mode, width, height, watermarked
 			FROM video.encode_formats format
-			INNER JOIN video.presets_encode_formats preset ON preset.preset_id = $1;`, p[i].PresetID)
+			INNER JOIN video.presets_encode_formats preset ON preset.encode_format_id = format.id
+			WHERE preset.preset_id = $1;`, p[i].PresetID)
+		if err != nil {
+			return nil, err
+		}
 	}
 	return p, nil
 }
