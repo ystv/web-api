a595cf3de8373525e283bf1baf817ca4aeca6a8c
rmil <rhys.milling@googlemail.com>
2020-08-20 04:21:54 +0100

    Positions new and update functions

--- controllers/v1/clapper/position.go
+++ controllers/v1/clapper/position.go
@@ -1,6 +1,7 @@
 package clapper
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
@@ -16,4 +17,37 @@ func PositionList(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, err)
 	}
 	return c.JSON(http.StatusOK, p)
+}
+
+// PositionNew handles creating a new position
+func PositionNew(c echo.Context) error {
+	p := position.Position{}
+	err := c.Bind(&p)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	err = position.New(&p)
+	if err != nil {
+		log.Printf("PositionNew failed: %+v", err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.NoContent(http.StatusCreated)
+}
+
+// PositionUpdate updates an existing position
+func PositionUpdate(c echo.Context) error {
+	p := position.Position{}
+	err := c.Bind(&p)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	err = position.Update(&p)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		log.Printf("PositionUpdate failed: %v", err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.NoContent(http.StatusOK)
 }
--- services/clapper/position/position.go
+++ services/clapper/position/position.go
@@ -11,7 +11,6 @@ type Position struct {
 	Name         string      `db:"name" json:"name"`
 	Description  null.String `db:"description" json:"description"`
 	Admin        bool        `db:"admin" json:"admin"`
-	Credible     bool        `db:"credible" json:"credible"` // TODO move credible to crew
 	PermissionID null.Int    `db:"permission_id" json:"permissionID"`
 }
 
@@ -19,10 +18,28 @@ type Position struct {
 func List() (*[]Position, error) {
 	p := []Position{}
 	err := utils.DB.Select(&p,
-		`SELECT position_id, name, description, admin, credible, permission_id
-		FROM event.positions;`) // TODO fix permissions
+		`SELECT position_id, name, description, admin, permission_id
+		FROM event.positions;`)
 	if err != nil {
 		return nil, err
 	}
 	return &p, nil
+}
+
+// New creates a position
+func New(p *Position) error {
+	_, err := utils.DB.Exec(
+		`INSERT INTO event.positions (name, description, admin, permission_id)
+		VALUES ($1, $2, $3, $4);`, &p.Name, &p.Description, &p.Admin, &p.PermissionID)
+	return err
+}
+
+// Update a position, uses the ID from the token
+func Update(p *Position) error {
+	_, err := utils.DB.Exec(
+		`UPDATE event.positions
+		SET name=$1, description=$2, admin=$3, permission_id=$4
+		WHERE position_id = $5`,
+		&p.Name, &p.Description, &p.Admin, &p.PermissionID, &p.PositionID)
+	return err
 }
