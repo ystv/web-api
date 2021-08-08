41e2bb1d1e7c10390d645435780a122883d51145
rmil <rhys.milling@googlemail.com>
2021-08-08 21:47:01 +0100

    Updated encode format to match latest schema

--- services/creator/encode/format.go
+++ services/creator/encode/format.go
@@ -11,7 +11,7 @@ func (s *Store) ListFormat(ctx context.Context) ([]encode.Format, error) {
 	e := []encode.Format{}
 	err := s.db.SelectContext(ctx, &e, `
 		SELECT id, name, description, mime_type, mode, width, height,
-		glob_args, src_args, dst_args, watermarked
+		arguments, file_suffix, watermarked
 		FROM video.encode_formats;`)
 	if err != nil {
 		return nil, err
--- services/creator/types/encode/encode.go
+++ services/creator/types/encode/encode.go
@@ -1,27 +1,26 @@
 package encode
 
-import "gopkg.in/guregu/null.v4"
-
 // Preset represents a group of encode formats. A video has a preset applied to it so
 // it can generate the video files so a video item.
 type (
 	Preset struct {
-		PresetID    int         `json:"id" db:"id"`
-		Name        string      `json:"name" db:"name"`
-		Description null.String `json:"description" db:"description"`
-		Formats     []Format    `json:"formats"`
+		PresetID    int      `json:"id" db:"id"`
+		Name        string   `json:"name" db:"name"`
+		Description string   `json:"description" db:"description"`
+		Formats     []Format `json:"formats"`
 	}
 	// Format represents the encode that is applied
 	// to a file.
 	Format struct {
-		FormatID    int         `json:"id" db:"id"`
-		Name        string      `json:"name" db:"name"`
-		Description null.String `json:"description" db:"description"`
-		MimeType    string      `json:"mimeType" db:"mime_type"`
-		Mode        string      `json:"mode" db:"mode"`
-		Width       int         `json:"width" db:"width"`
-		Height      int         `json:"height" db:"height"`
-		Arguments   string      `json:"arguments" db:"arguments"`
-		Watermarked bool        `json:"watermarked" db:"watermarked"`
+		FormatID    int    `json:"id" db:"id"`
+		Name        string `json:"name" db:"name"`
+		Description string `json:"description" db:"description"`
+		MimeType    string `json:"mimeType" db:"mime_type"`
+		Mode        string `json:"mode" db:"mode"`
+		Width       int    `json:"width" db:"width"`
+		Height      int    `json:"height" db:"height"`
+		Arguments   string `json:"arguments" db:"arguments"`
+		FileSuffix  string `json:"fileSuffix" db:"file_suffix"`
+		Watermarked bool   `json:"watermarked" db:"watermarked"`
 	}
 )
