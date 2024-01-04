3c660ac41fd44bf7550d6c69babd22a18fd5e8c5
Liam Burnand <liam.burnand@ystv.co.uk>
2024-01-04 21:36:01 +0000

    Fixing lint, don't care about error

--- utils/database.go
+++ utils/database.go
@@ -43,10 +43,10 @@ func Transact(db *sqlx.DB, txFunc func(*sqlx.Tx) error) (err error) {
 	}
 	defer func() {
 		if p := recover(); p != nil {
-			tx.Rollback()
+			_ = tx.Rollback()
 			panic(p) // re-throw panic after Rollback
 		} else if err != nil {
-			tx.Rollback() // err is non-nil; don't change it
+			_ = tx.Rollback() // err is non-nil; don't change it
 		} else {
 			err = tx.Commit() // err is nil; if Commit returns error update err
 		}
