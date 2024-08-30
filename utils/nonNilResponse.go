b095ce557c1d6f2d3ca3935ad0dd387887cd6847
Liam Burnand <liam.burnand@ystv.co.uk>
2024-08-30 19:08:30 +0100

    Adding a function to eliminate null or nil responses

---
+++ utils/nonNilResponse.go
@@ -0,0 +1,22 @@
+package utils
+
+import "reflect"
+
+func NonNil[T any](k T) T {
+	v := reflect.ValueOf(k)
+
+	switch v.Kind() {
+	case reflect.Slice:
+		if v.IsNil() {
+			return reflect.MakeSlice(v.Type(), 0, 0).Interface().(T)
+		}
+	case reflect.Ptr, reflect.Map, reflect.Chan, reflect.Func, reflect.Interface:
+		if v.IsNil() {
+			return reflect.New(v.Type().Elem()).Interface().(T)
+		}
+	default:
+		panic("unhandled default case")
+	}
+
+	return k
+}
