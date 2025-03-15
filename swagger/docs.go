2f0551b9374d980bf94c46fcf8672c97f00af1d3
Liam Burnand <liam.burnand@ystv.co.uk>
2025-03-15 07:49:37 +0000

    Left the debugging in

--- swagger/docs.go
+++ swagger/docs.go
@@ -5597,7 +5597,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8081",
+	Host:             "api.ystv.co.uk",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "YSTV Web API",
