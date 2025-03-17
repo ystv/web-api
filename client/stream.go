c250cbe5ef316cc705b05f44cbb4b2041c51f417
Liam Burnand <liam.burnand@ystv.co.uk>
2025-03-17 00:17:02 +0000

    Adding the remaining stream endpoints

--- client/stream.go
+++ client/stream.go
@@ -56,3 +56,73 @@ func (c *Client) FindStreamEndpoint(ctx context.Context, apiKey string, options 
 
 	return res, nil
 }
+
+func (c *Client) NewStreamEndpoint(ctx context.Context, apiKey string, options stream.EndpointNewEditDTO) (stream.Endpoint, error) {
+	u, err := url.Parse(c.BaseURL + "/v1/internal/streams")
+	if err != nil {
+		return stream.Endpoint{}, fmt.Errorf("invalid base URL: %w", err)
+	}
+
+	optionBytes, err := json.Marshal(options)
+	if err != nil {
+		return stream.Endpoint{}, err
+	}
+
+	buf := bytes.NewBuffer(optionBytes)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buf)
+	if err != nil {
+		return stream.Endpoint{}, err
+	}
+
+	var res stream.Endpoint
+	if err = c.sendRequest(req, apiKey, &res); err != nil {
+		return stream.Endpoint{}, err
+	}
+
+	return res, nil
+}
+
+func (c *Client) EditStreamEndpoint(ctx context.Context, apiKey string, endpointID int, options stream.EndpointNewEditDTO) (stream.Endpoint, error) {
+	u, err := url.Parse(fmt.Sprintf("%s/v1/internal/streams/%d", c.BaseURL, endpointID))
+	if err != nil {
+		return stream.Endpoint{}, fmt.Errorf("invalid base URL: %w", err)
+	}
+
+	optionBytes, err := json.Marshal(options)
+	if err != nil {
+		return stream.Endpoint{}, err
+	}
+
+	buf := bytes.NewBuffer(optionBytes)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, u.String(), buf)
+	if err != nil {
+		return stream.Endpoint{}, err
+	}
+
+	var res stream.Endpoint
+	if err = c.sendRequest(req, apiKey, &res); err != nil {
+		return stream.Endpoint{}, err
+	}
+
+	return res, nil
+}
+
+func (c *Client) DeleteStreamEndpoint(ctx context.Context, apiKey string, endpointID int) error {
+	u, err := url.Parse(fmt.Sprintf("%s/v1/internal/streams/%d", c.BaseURL, endpointID))
+	if err != nil {
+		return fmt.Errorf("invalid base URL: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, u.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	if err = c.sendRequest(req, apiKey, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
