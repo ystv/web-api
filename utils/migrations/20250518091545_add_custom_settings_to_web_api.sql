505239f169fcd959826ac8048c96b52e9c88c805
Liam Burnand <liam.burnand@ystv.co.uk>
2025-05-18 20:44:07 +0100

    Changing to jsonb type

--- utils/migrations/20250518091545_add_custom_settings_to_web_api.sql
+++ utils/migrations/20250518091545_add_custom_settings_to_web_api.sql
@@ -5,7 +5,7 @@ CREATE TABLE web_api.custom_settings
     setting_id    varchar                  not null
         constraint custom_settings_pk
             primary key,
-    value        varchar                  not null,
+    value        jsonb                  not null,
     public boolean default false
 );
 
